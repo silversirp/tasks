58c3b1df709e6b9a27eca5c647e0ffc96194a27f
Silver Sirp <silver.sirp@khk.ee>
2021-10-22 16:10:57 +0300

    Added single task delete functionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // event elements
 const form = document.querySelector('form');
+const taskList = document.querySelector('.collection');
 
 // events
 form.addEventListener('submit', addTask);
+taskList.addEventListener('click', deleteTask);
 
 function addTask(event) {
     //get form input data
@@ -12,13 +14,36 @@ function addTask(event) {
     //create <li> element
     const li = document.createElement('li');
     li.className = 'collection-item';
+
     //create text element
     const text = document.createTextNode(task);
     //add text to <li>
     li.appendChild(text);
+    //create <a> element
+    const link = document.createElement('a');
+    // add css class to link
+    link.className = 'secondary-content';
+    // set href attribute
+    link.setAttribute('href', '#');
+    // add text content to <a>
+    link.appendChild(document.createTextNode('X'));
+    // add a to li
+    li.appendChild(link);
     //add li to <ul>
     const ul = document.querySelector('.collection');
     ul.appendChild(li);
     event.preventDefault();
+}
 
+function deleteTask(event) {
+    //target näitab , mis elemendil klikati
+    //.textContent näitab ka teksti sisu ja meil on lingi sisuks X
+    //console.log(event.target.textContent);
+    if (event.target.textContent == 'X') {
+        if (confirm('Do you want to delete this task?')) {
+            //parentElement annab li elemendi, mitte enam targeti, mis oli a
+            event.target.parentElement.remove();
+        }
+
+    }
 }
