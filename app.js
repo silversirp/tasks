30ae376087e08a53c2f38a4f7bf760604775a2e9
Silver Sirp <silver.sirp@khk.ee>
2021-11-10 03:37:46 +0200

    Add task value to localStorage

--- app.js
+++ app.js
@@ -34,8 +34,23 @@ function addTask(event) {
     //add li to <ul>
     const ul = document.querySelector('.collection');
     ul.appendChild(li);
+    //save task
+    addTaskToLocalStorage(task);
     taskInput.value = '';
     event.preventDefault();
+}
+
+function addTaskToLocalStorage(task) {
+    console.log(task);
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    console.log(tasks);
 }
 
 function deleteTask(event) {
